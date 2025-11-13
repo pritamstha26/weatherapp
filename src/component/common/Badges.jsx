8d97ec430f9a367cc660b03c10c17f4d0a8b6d5d
pritamstha <pritamstha26@gmail.com>
2025-11-14 01:51:02 +0545

    feat: add Badge component

---
+++ src/component/common/Badges.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaCloudversify } from "react-icons/fa6";
+export default function Badges({ title = "Air Quality", value = "256" }) {
+  return (
+    <>
+      <div className="inline-flex flex-col  gap-2 rounded-2xl w-full      bg-white shadow-2xl px-6 py-2 text-md font-medium text-gray-400 inset-ring inset-ring-gray-400/20">
+        <div className="flex justify-center gap-2">
+          <FaCloudversify className="text-gray-600" />
+          <p className="text-gray-600">{title}</p>
+        </div>
+        <p className="font-bold text-center text-gray-600">{value}</p>
+      </div>
+    </>
+  );
+}
